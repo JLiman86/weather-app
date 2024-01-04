c8167f37152a88ab4e9368dc8fa9e5d12e877670
Jacek Liman <limaniec@gmail.com>
2024-01-04 15:26:33 +0100

    add icons

---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+import {
+  IoMdSunny,
+  IoMdRainy,
+  IoMdCloudy,
+  IoMdSnow,
+  IoMdThunderstorm,
+  IoMdSearch,
+} from "react-icons/io";
+
+import {
+  BsCloudHaze2Fill,
+  BsCloudDrizzleFill,
+  BsEye,
+  BsWater,
+  BsThermometer,
+  BsWind,
+} from "react-icons/bs";
+
+import {TbTemperatureCelsius} from 'react-icons/tb'
+import {ImSpinner8} from 'react-icons/im'
+
+
+
+
+function App() {
+
+ 
+
+
+  return <div className="bg-red-200">Hello World</div>;
+}
+
+export default App;
