a0cda16b878771f6be047b0b4f6fcaf08250b462
Jacek Liman <limaniec@gmail.com>
2024-01-09 16:01:33 +0100

    add center section

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
-import { IconType } from "react-icons";
-
 import {
   IoMdSunny,
   IoMdRainy,
@@ -28,6 +26,19 @@ const APIkey = "7478ace9dbf0d1f0e01be727c41388c6";
 
 type WeatherTypes = {
   [key: string]: unknown;
+  wind: {
+    speed: number;
+  };
+  visibility: number;
+  main: {
+    temp: number;
+    feels_like: number;
+    humidity: number;
+  };
+  name: string;
+  sys: {
+    country: string;
+  };
   weather: { description: string; icon: string; id: number; main: string }[];
 };
 
@@ -38,20 +49,26 @@ function App() {
   useEffect(() => {
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${APIkey}`;
 
-    axios.get(url).then((res) => setData(res.data));
+    axios.get(url).then((res) => {
+      setTimeout(() => {
+        setData(res.data);
+      }, 1000);
+    });
   }, [location]);
 
   if (!data) {
     return (
-      <div>
+      <div className="h-screen bg-gradientBg bg-center bg-no-repeat bg-cover flex justify-center items-center">
         <div>
-          <ImSpinner8 className="text-5xl animate-spin" />
+          <ImSpinner8 className="text-5xl animate-spin text-white" />
         </div>
       </div>
     );
   }
 
   let icon = <IoMdSunny />;
+  let celsius: number | string = data.main.temp - 273.15;
+  celsius = celsius.toFixed(1);
 
   switch (data.weather[0].main) {
     case "Clouds":
@@ -64,7 +81,7 @@ function App() {
       icon = <IoMdRainy />;
       break;
     case "Clear":
-      icon = <IoMdSunny />;
+      icon = <IoMdSunny className="text-yellow-300" />;
       break;
     case "Drizzle":
       icon = <BsCloudDrizzleFill />;
@@ -77,7 +94,79 @@ function App() {
       break;
   }
 
-  return <div>{icon}</div>;
+  const date = new Date();
+
+  return (
+    <div className="h-screen bg-gradientBg bg-center bg-no-repeat bg-cover flex justify-center items-center p-4">
+      <div className="max-w-[450px] h-[500px] w-full   ">
+        <div className="bg-black/20 rounded-full mb-4 px-4 py-4">tgrtgtr</div>
+        <div className="bg-black/20 rounded-2xl px-4 py-8">
+          <div className="flex gap-6 items-center">
+            <div className="text-8xl">{icon}</div>
+            <div className="flex-1 ">
+              <div className="text-2xl font-semibold">
+                {data.name},{data.sys.country}
+              </div>
+              <div>
+                {date.getUTCDate()}/{date.getUTCMonth() + 1}/
+                {date.getUTCFullYear()}
+              </div>
+            </div>
+          </div>
+
+          <div className="flex flex-col justify-center items-center h-[300px]">
+            <div className="flex items-center">
+              <div className="font-light text-9xl">{celsius}</div>
+              <div className="text-2xl">
+                <TbTemperatureCelsius />
+              </div>
+            </div>
+            <div>{data.weather[0].main}</div>
+          </div>
+          <div className="grid grid-cols-2 grid-rows-2 gap-y-5">
+            <div className="flex items-center gap-3">
+              <div>
+                <BsEye />
+              </div>
+              <div>
+                Visibility <span>{data.visibility / 1000} km</span>
+              </div>
+            </div>
+            <div className="flex items-center gap-3">
+              <div>
+                <BsThermometer />
+              </div>
+              <div>
+                Feels like
+                <span className="relative ml-2">
+                  {(data.main.feels_like - 273.15).toFixed(0)}
+                  <div className="absolute top-0 left-[20px]">
+                    <TbTemperatureCelsius />
+                  </div>
+                </span>
+              </div>
+            </div>
+            <div className="flex items-center gap-3">
+              <div>
+                <BsWater />
+              </div>
+              <div>
+                Humidity <span>{data.main.humidity} %</span>
+              </div>
+            </div>
+            <div className="flex items-center gap-3">
+              <div>
+                <BsWind />
+              </div>
+              <div>
+                Wind <span>{data.wind.speed} m/s</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
